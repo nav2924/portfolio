12e095bff5bdd6d33f988dfa12aa422ac5e70940
nav2924 <navi29012004@gmail.com>
2024-11-05 17:29:50 +0530

    POST URL

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -36,7 +36,7 @@ function ContactForm() {
     try {
       setIsLoading(true);
       const res = await axios.post(
-        `${process.env.NEXT_PUBLIC_APP_URL}/api/contact`,
+        `https://portfolio-alpha-kohl-61.vercel.app/api/contact`,
         userInput
       );
 
